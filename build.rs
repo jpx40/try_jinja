942ac941bc7d93112e0485011d1ebc0015615ee5
Jonas <92295347+jpx40@users.noreply.github.com>
2024-02-15 19:59:13 +0100

    update

--- build.rs
+++ build.rs
@@ -7,14 +7,7 @@ fn css_build() {
     //    println!("cargo:rerun-if-changed=css/input.css");
     std::process::Command::new("sh")
         .args([
-            "npx",
-            "lightningcss",
-            "--minify",
-            " --bundle",
-            "--targets",
-            "'>= 0.25%'",
-            "style/*",
-            "-o static/style/bundle.css",
+            "npx ightningcss --minify --bundle -targets '>= 0.25%' style/* -o static/style/bundle.css",
         ])
         .status()
         .expect("failed to execute npm run build");
