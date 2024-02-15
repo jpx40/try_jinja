49de617958e3da98843f08c5b504c19d292986a1
Jonas <92295347+jpx40@users.noreply.github.com>
2024-02-15 21:09:02 +0100

    update

--- build.rs
+++ build.rs
@@ -1,14 +1,83 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
 // Example custom build script.
 fn main() {
-    css_build();
+    build();
+}
+
+fn clear_css() {
+    if Path::new("static/style/bundle.css").exists() {
+        fs::remove_file("static/style/bundle.css").expect("failed to remove bundle.css");
+    }
 }
 
 fn css_build() {
     //    println!("cargo:rerun-if-changed=css/input.css");
+    // if Path::new("static/style/bundle.css").exists() {
+    //     clear_css();
+    // }
+
+    let home_path = Path::new("/home/jonas/")
+        .canonicalize()
+        .unwrap()
+        .to_owned()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let code_path = "code/rust/web/try_jinja";
+
+    let path: String = home_path + code_path;
+
+    let input = path.clone() + "style/*";
+    let output = path.clone() + "static/style/bundle.css";
+    println!("building css");
+    let cmd = "npx lightningcss --minify --bundle --targets '>= 0.25%' ./style/* --output-file ./static/style/bundle.css";
     std::process::Command::new("sh")
         .args([
-            "npx ightningcss --minify --bundle -targets '>= 0.25%' style/* -o static/style/bundle.css",
+            "npx",
+            "lightningcss",
+            "--minify",
+            "--bundle",
+            "--targets",
+            "'>= 0.25%''",
+            &input,
+            "--output-file",
+            &output,
         ])
         .status()
         .expect("failed to execute npm run build");
+}
+
+fn build() {
+    clear_css();
+    css_build();
+    js_build();
+}
+
+fn build_watch() {}
+
+fn js_build() {
+    let cmd = "esbuild js/* --bundle --minify  --outdir=./static/lib";
+
+    let home_path = Path::new("/home/jonas/")
+        .canonicalize()
+        .unwrap()
+        .to_owned()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let code_path = "code/rust/web/try_jinja";
+
+    let path: String = home_path + code_path;
+    println!("{:?}", path);
+    println!("building js");
+
+    let input: String = path.clone() + "js/*";
+    let output: String = "--outdir=".to_string() + &path + "static/lib";
+    Command::new("sh")
+        .args(["esbuild", &input, "--bundle", "--minify", &output])
+        .status()
+        .expect("esbuild failed");
 }
---
+++ js/test.js
@@ -0,0 +1,3 @@
+import * as lodash from "lodash";
+
+lodash.chunk(["1", "2", "3", "4"], 2);
