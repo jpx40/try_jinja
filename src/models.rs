90148a690555ed21a006399930edc3a6d430e3ba
Jonas <92295347+jpx40@users.noreply.github.com>
2024-02-17 16:33:22 +0100

    update

--- src/models.rs
+++ src/models.rs
@@ -3,7 +3,7 @@ use diesel::prelude::*;
 
 #[derive(Insertable, Queryable, Selectable)]
 #[diesel(table_name = crate::schema::packages)]
-//#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 #[derive(Debug, Clone)]
 pub struct Package {
